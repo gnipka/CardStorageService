df221f6750e75359eba6eedc221a78da9c172b5f
gnipka <gnipka29@gmail.com>
2023-03-07 20:12:44 +0300

    lesson2

--- CardStorageService.Data/CardStorageServiceDbContext.cs
+++ CardStorageService.Data/CardStorageServiceDbContext.cs
@@ -6,6 +6,8 @@ namespace CardStorageService.Data
     {
         public virtual DbSet<Client> Clients { get; set; }
         public virtual DbSet<Card> Cards { get; set; }
+        public virtual DbSet<Account> Accounts { get; set; }
+        public virtual DbSet<AccountSession> AccountSessions { get; set; }
 
         public CardStorageServiceDbContext(DbContextOptions options) : base(options) { }
     }
---
+++ JwtSample/UserService.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JwtSample
+{
+    internal class UserService
+    {
+        private const string SecretCode = "TopSecretTopSecret";
+
+        private Dictionary<string, string> _users = new Dictionary<string, string>()
+        {
+            {"root1", "test" }, // 0
+            {"root2", "test" }, // 1
+            {"root3", "test" }, // 2
+            {"root4", "test" } // 3
+        };
+
+        public string Authenticate(string user, string password)
+        {
+            if (string.IsNullOrWhiteSpace(user) || string.IsNullOrWhiteSpace(password))
+            {
+                return string.Empty;
+            }
+            int i = 0;
+            foreach (KeyValuePair<string, string> pair in _users)
+            {
+                if (string.CompareOrdinal(pair.Key, user) == 0 && string.CompareOrdinal(pair.Value, password) == 0)
+                {
+                    return GenerateJwtToken(i);
+                }
+                i++;
+            }
+
+            return string.Empty;
+        }
+
+        private string GenerateJwtToken(int id)
+        {
+            JwtSecurityTokenHandler jwtSecurityTokenHandler= new JwtSecurityTokenHandler();
+            byte[] key = Encoding.ASCII.GetBytes(SecretCode);
+
+            SecurityTokenDescriptor securityTokenDescriptor = new SecurityTokenDescriptor();
+            securityTokenDescriptor.Expires = DateTime.UtcNow.AddMinutes(15); // время жизни токена (в данном случае 15)
+            securityTokenDescriptor.SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature);
+            securityTokenDescriptor.Subject = new System.Security.Claims.ClaimsIdentity(new Claim[] { 
+                new Claim(ClaimTypes.NameIdentifier, id.ToString())
+            });
+
+            SecurityToken securityToken = jwtSecurityTokenHandler.CreateToken(securityTokenDescriptor);
+            return jwtSecurityTokenHandler.WriteToken(securityToken);
+        }
+    }
+}
